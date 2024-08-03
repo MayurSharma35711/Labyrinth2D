5d2fb763e7411e47b80ebe6dd52acfabe256d7ed
Mayur Sharma <msharma12@cs.uchicago.edu>
2024-08-03 14:01:23 -0400

    Put everything in 1 file before we figure out dependency stuff

--- new_game.js
+++ new_game.js
@@ -1,20 +1,168 @@
-const imager = require("./SourceCode/imager");
-// init();
+// ------------------------- INITIALIZE -----------------
 
-var cvs = document.createElement('canvas'),
-    img = document.getElementsByTagName("img")[0];   // your image goes here
-    // img = $('#yourImage')[0];                     // can use jquery for selection
-cvs.width = img.width; cvs.height = img.height;
-var ctx = cvs.getContext("2d");
-ctx.drawImage(img,0,0,cvs.width,cvs.height);
-var idt = ctx.getImageData(0,0,cvs.width,cvs.height);
+function init(){
+    let type = "WebGL";
+    if(!PIXI.utils.isWebGLSupported()){
+      type = "canvas";
+    }
+  
+    PIXI.utils.sayHello(type);
+    app = new PIXI.Application({width: 256, height: 256, autoResize: true});
+    document.body.style.marginTop = 0;
+      document.body.style.marginLeft = 0;
+      document.body.style.marginBottom = 0;
+      document.body.style.marginUp = 0;
+    // screenAdjust();
+    // console.log(app.width);
+    document.body.appendChild(app.view);
+  
+    // gameScene = new PIXI.Container();
+    // app.stage.addChild(gameScene);
+    // var texture = PIXI.Texture.fromImage('Imgs/Stars.jpg');
+    // var background = new PIXI.Sprite(texture);
+    // gameScene.addChild(background);
+  
+    app.renderer.backgroundColor = 0x061639;
+    app.ticker.add(delta => gameLoop(delta));
+    t1 = Date.now();
+  
+    // document.addEventListener('keydown', keyStart);
+    // document.addEventListener('keyup', keyEnd);
+  }
 
-// usage
-imager.getPixel(idt, 852);  // returns array [red, green, blue, alpha]
-imager.getPixelXY(idt, 1,1); // same pixel using x,y
+// ------------------------- Vivek Map Code --------------
+let max_world_up = 10;
+let max_world_left = 10;
+let max_world_right = 10;
+let max_world_down = 10;
 
-imager.setPixelXY(idt, 1,1, 0,0,0,255); // a black pixel
-ctx.putImageData(idt, 0,0);  // 0,0 is xy coordinates
-img.src = cvs.toDataURL();
-document.body.appendChild(cvs)
-console.log("hi")
+
+const map = [];
+const rand = [0, 0];
+let max_biome_size = 0;
+let biome_num = 5;
+for(let i = 0;i < 20;i++)
+{
+    for(let k = 0;k < 20;k++)
+    {
+        map[i][k] = 0; //0 means no biome set
+    }
+}
+let num_left = 400; //Counts how many blocks don't have set biomes
+function getRandBiomeSize()
+{
+    while(max_biome_size >= 20 || max_biome_size <= 5) //This assumes the max biome size is 20 and the min biome size is 5
+    {
+        max_biome_size = Math.random() % 20;
+    }
+}
+function genBiome()
+{
+    getRandBiomeSize();
+    let biome = Math.random() % biome_num;
+    let dir; //direction it will go
+    let test = 1; //tests whether the direction works
+    let fail = 0;
+    rand[0] = Math.random() % 20;
+    rand[1] = Math.random() % 20;
+    for(let i = 0;i < max_biome_size;i++)
+    {
+        dir = Math.random() % 4;
+        switch(dir)
+        {
+        case 0: //Right
+            if(map[rand[0]][rand[1] + 1].biome != 0 && !(rand[1] + 1 >= 20))
+            {
+                test = 0;
+                fail++;
+            }
+            else
+            {
+                map[rand[0]][++rand[1]].setBiome(biome);
+                test = 1;
+                num_left--;
+                fail = 0;
+                break;
+            }
+        case 1: //Left
+            if(map[rand[0]][rand[1] - 1].biome != 0 && !(rand[1] - 1 <= 20))
+            {
+                test = 0;
+                fail++;
+            }
+            else
+            {
+                map[rand[0]][--rand[1]].setBiome(biome);
+                test = 1;
+                num_left--;
+                fail = 0;
+                break;
+            }
+        case 2: //Down
+            if(map[rand[0] + 1][rand[1]].biome != 0 && !(rand[0] + 1 >= 20))
+            {
+                test = 0;
+                fail++;
+            }                
+            else
+            {
+                map[++rand[0]][rand[1]].setBiome(biome);
+                test = 1;
+                num_left--;
+                fail = 0;
+                break;
+            }
+        case 3: //Up
+            if(map[rand[0] - 1][rand[1]].biome != 0 && !(rand[0] - 1 <= 20))
+            {
+                test = 0;
+                fail++;
+            }
+            else
+            {
+                map[--rand[0]][rand[1]].setBiome(biome);
+                test = 1;
+                num_left--;
+                fail = 0;
+                break;
+            }
+        }
+        if(test >= 4)
+        {
+            break;
+        }
+    }
+    max_biome_size = 0;
+}
+function setWorld(times)
+{
+    for(let i = 0;i < times;i++)
+    {
+        genBiome();
+    }
+}
+function printWorldBiomes()
+{
+    for(let i = 0;i < 20;i++)
+    {
+        for(let k = 0;k < 20;k++)
+        {
+            console.log(map[i][k].biome + " ");
+        }
+        console.log("\n");
+    }
+}
+setWorld();
+// printWorldBiomes();
+
+
+
+
+init()
+
+var texture = new PIXI.RenderTexture(renderer, 16, 16);
+var graphics = new PIXI.Graphics();
+graphics.beginFill(0x44FFFF);
+graphics.drawCircle(8, 8, 8);
+graphics.endFill();
+texture.render(graphics);
