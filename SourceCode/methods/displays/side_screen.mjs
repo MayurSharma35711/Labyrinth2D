7d9ec38dcb771c1efb97e3ccb5da53552e80fec8
Mayur Sharma <msharma12@cs.uchicago.edu>
2025-08-24 14:55:11 -0400

    original cards working

--- SourceCode/methods/displays/side_screen.mjs
+++ SourceCode/methods/displays/side_screen.mjs
@@ -86,6 +86,7 @@ export function init_all_player_cards(players, locx, locy, sizex, sizey) {
     let health_bars = []
     let name_text = []
     let stepcounters = []
+    let staters = []
     for (let l = 0; l < players.length; l++) {
         let index = players[l].player_ind
         let output = init_player_card(players[l], locx + index * (sizex) * 1 - 2 * sizex + 0.1*sizex, locy, 0.9 * sizex, 0.9*sizey)
@@ -93,10 +94,11 @@ export function init_all_player_cards(players, locx, locy, sizex, sizey) {
         health_bars.push(output[1])
         name_text.push(output[2])
         stepcounters.push(output[3])
+        staters.push(output[4])
         player_info.addChild(output[0])
     }
     
-    return [player_info, cards, health_bars, name_text, stepcounters]
+    return [player_info, cards, health_bars, name_text, stepcounters, staters]
 }
 
 
@@ -108,7 +110,7 @@ export function init_player_card(player, locx, locy, sizex, sizey) {
     player_card.position.set(locx, locy); // Center the menu
     player_card.pivot.set(player_card.width / 2, player_card.height / 2);
 
-    let health_bar = init_health_bar(player, sizex / 2 + sizex / 30 ,  sizey, sizex, sizey)
+    let health_bar = init_health_bar(player, sizex / 20 ,  sizey * 19 / 24, sizex * 18 / 20, sizey / 10)
 
     const bndy_rect = new PIXI.Graphics();
     bndy_rect.rect(0, 0, sizex, sizey);
@@ -142,9 +144,19 @@ export function init_player_card(player, locx, locy, sizex, sizey) {
         fill: 0xffffff,
     });
     player_name.anchor.set(0); // Center the text within its bounds
-    player_name.position.y = sizey * 2 / 24; // Position relative to menuContainer's center
+    player_name.position.y = sizey * 1.5 / 24; // Position relative to menuContainer's center
     player_name.position.x = sizex / 20
     player_card.addChild(player_name);
+
+    const player_stats = new PIXI.Text( "stats \neffects", {
+        fontFamily: 'Arial',
+        fontSize: 16,
+        fill: 0xffffff,
+    });
+    player_stats.anchor.set(0); // Center the text within its bounds
+    player_stats.position.y = sizey * 8 / 24; // Position relative to menuContainer's center
+    player_stats.position.x = sizex / 20
+    player_card.addChild(player_stats);
 
     const stepper = new PIXI.Text(speeder, {
         fontFamily: 'Arial',
@@ -152,11 +164,27 @@ export function init_player_card(player, locx, locy, sizex, sizey) {
         fill: 0x00ff00,
     });
     stepper.anchor.set(1,0); // Center the text within its bounds
-    stepper.position.y = sizey * 2 / 24; // Position relative to menuContainer's center
+    stepper.position.y = sizey * 1.5 / 24; // Position relative to menuContainer's center
     stepper.position.x = sizex * 19 / 20
     player_card.addChild(stepper);
 
-    return [player_card, health_bar[1], player_name, stepper]
+    const sprite_bkg = new PIXI.Graphics()
+    sprite_bkg.rect(sizex * 13 / 20 , sizey * 7/24 , sizex * 6 / 20 , sizex * 6 / 20)
+    // bndy_rect.rect(-app.screen.width / 15 - app.screen.width / 400, -app.screen.width / 10 - app.screen.width / 400 
+    //     , app.screen.width / 8 + app.screen.width / 200, app.screen.width / 70 + app.screen.width / 200);
+    sprite_bkg.fill(0xFFFFFF);
+    player_card.addChild(sprite_bkg)
+
+    const sprite = new PIXI.Sprite(player.sprite.texture)
+    sprite.x = sizex * 13 / 20 + sizex * 1 / 40
+    sprite.y = sizey * 7/24 + sizex * 1 / 40
+    sprite.width = sizex * 5 / 20
+    sprite.height = sizex * 5 / 20
+    player_card.addChild(sprite)
+    
+
+
+    return [player_card, health_bar[1], player_name, stepper, player_stats]
     // this contains the player name, player sprite, player health bar, and maybe some of their status effects 
     // probably need to make a class just for status effects
     
@@ -176,23 +204,27 @@ function init_health_bar(player, locx, locy, sizex, sizey){
     // const ind_bar = new PIXI.Container()health_bar
 
     const bndy_rect = new PIXI.Graphics();
-    bndy_rect.rect(-app.screen.width / 15 - app.screen.width / 400, -app.screen.width / 10 - app.screen.width / 400 
-        , app.screen.width / 8 + app.screen.width / 200, app.screen.width / 70 + app.screen.width / 200);
+    bndy_rect.rect(-1,-1, sizex + 2, sizey + 2)
+    // bndy_rect.rect(-app.screen.width / 15 - app.screen.width / 400, -app.screen.width / 10 - app.screen.width / 400 
+    //     , app.screen.width / 8 + app.screen.width / 200, app.screen.width / 70 + app.screen.width / 200);
     bndy_rect.fill(0x000000);
     // bkg_rect.lineStyle(100, 0x000000, 1)
     // health_bars.addChild(bndy_rect)
 
     const bkg_rect = new PIXI.Graphics();
-    bkg_rect.rect(-app.screen.width / 15, -app.screen.width / 10 , app.screen.width / 8, app.screen.width / 70);
-    bkg_rect.fill(0x33FF66);
+    bkg_rect.rect(0,0, sizex, sizey)
+    // bkg_rect.rect(-app.screen.width / 15, -app.screen.width / 10 , app.screen.width / 8, app.screen.width / 70);
+    bkg_rect.fill(0xFF99BB);
     // bkg_rect.lineStyle(100, 0x000000, 1)
     // health_bars.addChild(bkg_rect)
 
     const health_rect = new PIXI.Graphics();
-    health_rect.rect(-app.screen.width / 15 + app.screen.width / 200, -app.screen.width / 10 + app.screen.width / 420, 
-        (app.screen.width / 8 - app.screen.width / 100) * player.health / 10, app.screen.width / 105);
+    health_rect.rect(0 , 0, sizex * player.health / 10, sizey)
+    // health_rect.rect(-app.screen.width / 15 + app.screen.width / 200, -app.screen.width / 10 + app.screen.width / 420, 
+    //     (app.screen.width / 8 - app.screen.width / 100) * player.health / 10, app.screen.width / 105);
     //  ; ;  
-    health_rect.fill(0xFF3366);
+    health_rect.fill(0xFF3366)
+    // health_rect.fill(0x0000ff)
     // health_bars.addChild(health_rect)
 
     health_bar.addChild(bndy_rect)
@@ -233,8 +265,8 @@ export function update_player_cards(){
             indiv_steps[k].style.fill = 0x00ff00
         }
         let init_width = indiv_health[k].width
-        indiv_health[k].width = Math.max(0, (app.screen.width / 8 - app.screen.width / 100) * players[k].health / 10)
-        indiv_health[k].x = indiv_health[k].x - (init_width - indiv_health[k].width) / 2
+        indiv_health[k].width = Math.max(0, init_width * players[k].health / 10)
+        // indiv_health[k].x = indiv_health[k].x - (init_width - indiv_health[k].width) / 2
     }
     app.stage.removeChild(tot_cards.item[0])
     app.stage.addChild(tot_cards.item[0])
